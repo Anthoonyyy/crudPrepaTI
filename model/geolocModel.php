4111049018743f31cf21942362edc1fde6bf98b3
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-22 18:43:45 +0200

    Création de la fonction getOneGeolocByid qui charge tous les champs d'un element grâce à son id

--- model/geolocModel.php
+++ model/geolocModel.php
@@ -15,3 +15,25 @@ function getAllGeoloc(PDO $connection): array|string
         return $e->getMessage();
     }
 }
+
+// Fonction qui charge tous les champs d'un élément de geoloc grâce à on idgeoloc
+// Renvoie false en cas d'échec ou un message d'erreur sql
+// Renvoie un tableau associatif si true
+
+function getOneGeolocById(PDO $db, int $id): string|bool|array
+{
+    $sql = "SELECT * FROM `geoloc` WHERE `idgeoloc` = :id";
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam("id", $id, PDO::PARAM_INT);
+
+    try {
+        $stmt->execute();
+        if ($stmt->rowCount() === 0) return false;
+        $results = $stmt->fetch();
+        $stmt->closeCursor();
+        return $results;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
