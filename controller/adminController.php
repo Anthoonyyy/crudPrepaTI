16360f2f582f81a2ba509e43748baba09b37e2a6
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-24 19:42:37 +0200

    Vérification si l'update est ok

--- controller/adminController.php
+++ controller/adminController.php
@@ -65,13 +65,6 @@ if (isset($_GET['delete']) && ctype_digit($_GET['delete'])) {
 }
 
 
-
-
-
-
-
-
-
 //si on a cliqué sur update et qu'on'accepte que les chiffres dans le string ['update']
 if (isset($_GET['update']) && ctype_digit($_GET['update'])) {
     //conversion en int
@@ -79,25 +72,34 @@ if (isset($_GET['update']) && ctype_digit($_GET['update'])) {
 
     //Si on a modifier le formulaire (pas obligatoire de vérifier tous les champs, mais dans le isset, la virgule vaut &&)
     if (isset(
-        $_POST['idgeoloc'],
         $_POST['title'],
         $_POST['geolocdesc'],
         $_POST['latitude'],
         $_POST['longitude']
     )) {
         // vérification de valeurs
-        $idgeoloc = (int) $_POST['idgeoloc'];
+        $idgeoloc = $idUpdate;
         $title  = htmlspecialchars(strip_tags(trim($_POST['title'])), ENT_QUOTES);
         $geolocdesc  = htmlspecialchars(trim($_POST['geolocdesc']), ENT_QUOTES);
         $latitude = (float) $_POST['latitude'];
         $longitude = (float) $_POST['longitude'];
 
         //fonction qui update la mise à jour
-        //$update =  updateOneGeolocByID($db,$idgeoloc);
+        $update =  updateOneGeolocByID($db, $idgeoloc, $title, $geolocdesc, $latitude, $longitude);
         //var_dump($update);
+
+        //Si l'update est bon
+        if ($update === true) {
+            header("Location: ./");
+            exit();
+        } elseif ($update === false) {
+            $errorUpdate = "Cet article n'a pas été modifié";
+        } else {
+            $errorUpdate = $update;
+        }
     }
     //Chargement de l'article pour l'update
-    // $getOneGeoLoc = getOneGeolocByID($db,$idUpdate);
+    $getOneGeoLoc = getOneGeolocByID($db, $idUpdate);
     // var_dump($getOneGeoLoc);
 
     //chargement de la vue
