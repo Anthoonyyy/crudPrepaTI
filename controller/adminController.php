3815ed59dfd4c1a8d5f6d12981457f7168b99002
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-24 19:36:30 +0200

    Vérification si on a cliqué sur supprimé avec isset(['delete']

--- controller/adminController.php
+++ controller/adminController.php
@@ -36,6 +36,42 @@ if (isset($_GET['create'])) {
     include "../view/admin/admin.insert.view.html.php";
 }
 
+// si on a cliqué sur supprimer un lieu
+if (isset($_GET['delete']) && ctype_digit($_GET['delete'])) {
+
+    //conversion en int
+    $idDelete = (int) $_GET['delete'];
+
+
+    // si on a validé la suppression
+    if (isset($_GET['ok'])) {
+        $delete = deleteOneGeolocById($db, $idDelete);
+        if ($delete === true) {
+            header("Location: ./");
+            exit();
+        } elseif ($delete === false) {
+            $error = "Problème avec cette suppression";
+        } else {
+            $error = $delete;
+        }
+    }
+
+    // chargement de l'article pour la suppression
+    $getOneGeoLoc = getOneGeolocById($db, $idDelete);
+
+    //chargement de la vue
+    include "../view/admin/admin.delete.view.html.php";
+    exit();
+}
+
+
+
+
+
+
+
+
+
 //si on a cliqué sur update et qu'on'accepte que les chiffres dans le string ['update']
 if (isset($_GET['update']) && ctype_digit($_GET['update'])) {
     //conversion en int
