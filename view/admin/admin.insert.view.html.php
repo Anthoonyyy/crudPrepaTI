8a52f7e4dbc6557bbf5cfbe38b470b37c3760cf8
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-25 17:57:27 +0200

    Ajout lien css et js bootstrap page insert

--- view/admin/admin.insert.view.html.php
+++ view/admin/admin.insert.view.html.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insertion d'un lieu</title>
     <link rel="stylesheet" href="css/style.css">
+    <!--Lien CSS Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -27,6 +29,8 @@
             <input type="submit" value="Insérer">
         </form>
     </div>
+    <!--Lien JS Bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
