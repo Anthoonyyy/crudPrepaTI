f334ef1a4e5909bd4324bb4fea5a2aa10f193995
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-25 17:52:33 +0200

    Utilisation d'un include pour incorporer la navbar

--- view/admin/admin.homepage.view.html.php
+++ view/admin/admin.homepage.view.html.php
@@ -16,29 +16,10 @@
 <body>
     <h1 class="text-center mt-4">Accueil de l'administration</h1>
 
+    <?php
+    include "inc/navbar.php";
+    ?>
 
-    <nav class="navbar navbar-expand-sm mt-5">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="./">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?create">Ajouter un lieu</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?disconnect">Déconnexion</a>
-                    </li>
-
-                </ul>
-            </div>
-        </div>
-    </nav>
     <div id="content">
         <h3 class="text-center mt-5">Administration des datas</h3>
         <?php
