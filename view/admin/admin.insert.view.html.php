bfe752268aee2942159133ff1e0adc7f29d0948e
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-25 18:59:29 +0200

    Mise en place form insertion mais probleme a corriger insertion

--- view/admin/admin.insert.view.html.php
+++ view/admin/admin.insert.view.html.php
@@ -11,23 +11,37 @@
 </head>
 
 <body>
-    <h1>Insertion d'un lieu</h1>
-    <nav>
-        <ul>
-            <li><a href="./">Accueil de l'administration</li>
-            <li><a href="?create">Ajouter un lieu</a></li>
-            <li><a href="?disconnect">Déconnexion</a></li>
-        </ul>
-    </nav>
+    <h1 class="text-center mt-4">Insertion d'un lieu</h1>
+    <?php
+    include "inc/navbar.php";
+    ?>
     <div id="content">
-        <h2>Ajoutez votre lieu</h2>
-        <form method="POST" name="geo" action="">
-            <input type="text" name="title" value="" placeholder="title" required><br>
-            <textarea name="geolocdesc" placeholder="description"></textarea><br>
-            <input type="number" placeholder="latitude" name="latitude" step="0.000000001" value="" required>
-            <input type="number" name="longitude" placeholder="longitude" step="0.000000001" value="" required>
-            <input type="submit" value="Insérer">
-        </form>
+        <h2 class="text-center mt-5">Ajoutez votre lieu</h2>
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <form method="POST" name="geo" action="">
+                    <div class="mb-3">
+                        <label for="titre" class=" form-label">Titre</label>
+                        <input type="text" name="title" placeholder="Titre" class="form-control" id="titre" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="desc" class="form-label">Description</label>
+                        <textarea name="geolocdesc" class="form-control" id="desc" rows="3" placeholder="description"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="latitude" class="form-label">Latitude</label>
+                        <input type="number" placeholder="latitude" name="latitude" step="0.000000001" class="form-control" id="latitude" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="longitude" class="form-label">Longitude</label>
+                        <input type="number" name="longitude" placeholder="longitude" step="0.000000001" class="form-control" id="longitude" required>
+                    </div>
+                    <div class="mb-3 text-center">
+                        <input type="submit" value="Insérer" class="btn btn-outline-primary">
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
     <!--Lien JS Bootstrap-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
